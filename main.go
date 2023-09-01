3ce8ab471e52744bc3e7a4b671ff9f7677e061c6
Hayden McAlister <haydenmcalister49@gmail.com>
2023-09-02 02:52:35 +1200

    Remove asset server to avoid failing go test

--- main.go
+++ main.go
@@ -1,15 +1,9 @@
 package main
 
 import (
-	"embed"
-
 	"github.com/wailsapp/wails/v2"
 	"github.com/wailsapp/wails/v2/pkg/options"
-	"github.com/wailsapp/wails/v2/pkg/options/assetserver"
 )
-
-//go:embed all:frontend/dist
-var assets embed.FS
 
 func main() {
 	// Create an instance of the app structure
@@ -17,12 +11,9 @@ func main() {
 
 	// Create application with options
 	err := wails.Run(&options.App{
-		Title:  "EventTrackerApp",
-		Width:  1024,
-		Height: 768,
-		AssetServer: &assetserver.Options{
-			Assets: assets,
-		},
+		Title:            "EventTrackerApp",
+		Width:            1024,
+		Height:           768,
 		BackgroundColour: &options.RGBA{R: 27, G: 38, B: 54, A: 1},
 		OnStartup:        app.startup,
 		Bind: []interface{}{
