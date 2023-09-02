cf00842ef0acd44fe72991777962467d3ebb220f
Hayden McAlister <haydenmcalister49@gmail.com>
2023-09-03 01:58:44 +1200

    Update frontend with skeleton.dev start

---
+++ frontend/src/routes/+page.svelte
@@ -0,0 +1,32 @@
+<script>
+	import Header from "./header.svelte";
+</script>
+
+<div class="container h-full mx-auto p-12">
+	<Header />
+
+	<div
+		id="body"
+		class="container h-full mx-auto flex justify-center items-center pt-12"
+	>
+		<div class="space-y-5">
+			<p>Start by exploring:</p>
+			<ul>
+				<li>
+					<code class="code">/src/routes/+layout.svelte</code> - barebones
+					layout
+				</li>
+				<li>
+					<code class="code">/src/app.postcss</code> - app wide css
+				</li>
+				<li>
+					<code class="code">/src/routes/+page.svelte</code> - this page,
+					you can replace the contents
+				</li>
+			</ul>
+		</div>
+	</div>
+</div>
+
+<style>
+</style>
